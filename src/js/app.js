a58364eab990229a92994d16c37923a436efa6f4
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-11-27 21:33:00 -0600

    Organized utility classes and css

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,31 @@
 'use strict';
+/*----------------------------------------------------------->
+
+<------------------------------------------------------------*/
 
 function select(selector, scope = document) {
   return scope.querySelector(selector);
 }
-
 function selectAll(selector, scope = document) {
   return scope.querySelectorAll(selector);
 }
-
 function listen(event, element, callback) {
   return element.addEventListener(event, callback);
 }
 
+/*----------------------------------------------------------->
+
+<------------------------------------------------------------*/
 
 const navBar = select(".nav-wrapper");
 const heroBanner = select(".hero-banner");
-const contactButton = select(".contact-btn");
 const contactModal = select(".contact");
 const showContact = select(".show-contact");
-
-
 const headerSwitch = heroBanner.offsetHeight;
+
+/*----------------------------------------------------------->
+
+<------------------------------------------------------------*/
 
 listen("scroll", window, () => {
 	const trigger = window.scrollY;
@@ -30,10 +35,6 @@ listen("scroll", window, () => {
 	} else {
 		navBar.classList.remove("visible");
 	}
-});
-
-listen('click', contactButton, () => {
-	contactModal.classList.add("visible");
 });
 
 listen('click', showContact, () => {
@@ -46,4 +47,3 @@ listen('click', showContact, () => {
 			showContact.innerText = "Contact";  // Change to Contact when modal is hidden
 	}
 });
-
