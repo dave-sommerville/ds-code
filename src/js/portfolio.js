43c40ae319d3c42db5425565eed072e921ffb5f9
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-06-21 11:58:55 -0500

    Portfolio objects fully populated

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -23,6 +23,7 @@ const rightButton = select('.right-btn');
 
 
 const infoDisplay = select('.info-display');
+const portfolioWrapper = select('.portfolio-wrapper');
 const portfolioPreview = select('.portfolio-preview');
 const portfolioTitle = select('.portfolio-title');
 const portfolioDescription = select('.portfolio-description');
@@ -36,19 +37,24 @@ class Portfolio {
   #imgUrl ='';
   #title = '';
   #description = '';
-  constructor(url, title, desc) {
-    this.imgUrl = url;
+  #linkUrl = '';
+  constructor(img, title, desc,link) {
+    this.imgUrl = img;
     this.title = title;
     this.description = desc;
+    this.linkUrl = link;
   }
-  set imgUrl(url) {
-    this.#imgUrl = url;
+  set imgUrl(img) {
+    this.#imgUrl = img;
   }
   set title(name) {
     this.#title = name;
   }
   set description(desc) {
     this.#description = desc;
+  }
+  set linkUrl(link) {
+    this.#linkUrl = link;
   }
   get imgUrl() {
     return this.#imgUrl;
@@ -59,13 +65,63 @@ class Portfolio {
   get description() {
     return this.#description;
   }
+  get linkUrl() {
+    return this.#description;
+  }
 }
-const blackjack = new Portfolio("./src/img/blackjack-dave-sommerville-github.jpg", "Blackjack", "My online version of the popular card game Blackjack");
-const scriptScavengers = new Portfolio("", "", "");
-const designItems = [blackjack, scriptScavengers];
-const gameItems = [];
-let portfolioItems;
+const blackjack = new Portfolio(
+  "./src/img/blackjack-dave-sommerville-github.jpg", 
+  "Blackjack", 
+  "My online version of the popular card game Blackjack. It features deal animations and a persistant player bank.", 
+  "https://dave-sommerville.github.io/blackjack/"
+);
 
+const scriptScavengers = new Portfolio(
+  "./src/img/script-scavengers-dave-sommerville-github.jpg", 
+  "Script Scavengers", 
+  "Fast typing game with a desert scavenger theme. Including a highscore list.", 
+  "https://dave-sommerville.github.io/script-scavengers/"
+);
+
+const codeBreaker = new Portfolio(
+  "./src/img/code-breaker-dave-sommerville-github.jpg",
+  "Code Breaker",
+  "Based on the game Mastermind, players can test their logical deducation skills and track their times.", 
+  "https://dave-sommerville.github.io/code-breaker/"
+);
+
+const booogle = new Portfolio(
+  "./src/img/boogle-dave-sommerville-github.jpg",
+  "Booogle", 
+  "A handy online Boggle display for large groups or travelling."
+  + "Letter probabilities reflect the physical version.", 
+  "https://dave-sommerville.github.io/boogle/"
+);
+
+const meMyself = new Portfolio(
+  "./src/img/me-myself-and-eye-dave-sommerville-github.jpg", 
+  "Me, Myself, and Eye", 
+  "A solipistic social media site where who you are is up to you.", 
+  "https://dave-sommerville.github.io/social-media-app/"
+);
+
+const opalOnyx = new Portfolio(
+  "./src/img/opal-and-onyx-dave-sommerville-github.jpg",
+  "Opal & Onyx",
+  "Collaboration project exploring luxury ecommerce design.",
+  "https://dave-sommerville.github.io/opal-and-onyx/"
+);
+
+const hiveMind = new Portfolio(
+  "./src/img/hivemind-dave-sommerville-github.jpg", 
+  "HiveMind", 
+  "Collaboration project creating our own version of a social media site. Also where the obsession with hexagons started for me.", 
+  "https://dave-sommerville.github.io/hivemind/"
+);
+
+const designItems = [opalOnyx, hiveMind, meMyself];
+const gameItems = [blackjack, scriptScavengers, codeBreaker, booogle];
+let portfolioItems;
 
 function displayPortfolioItem(index) {
   let item = portfolioItems[index];
@@ -75,33 +131,62 @@ function displayPortfolioItem(index) {
 }
 
 let currentIndex = 0;
-let totalEntries = 2;
 
-function adjustIndex(index, operator) {
-  if(operator === '+') {
+portfolioItems = designItems;
+displayPortfolioItem(currentIndex);
+
+function adjustIndex(index, operator, entryLimit) {
+  if (operator === '+') {
     index++;
   } else if (operator === '-') {
     index--;
   } else {
     console.log("Please enter a + or -");
   }
-  // if (index >= (totalEntries)) {
-  //   index = 0;
-  // }
+
+  if (index >= entryLimit) {
+    index = 0;
+  }
+  if (index < 0) {
+    index = entryLimit - 1;
+  }
   return index;
 }
 
-listen("click", gameButton, () => {
+function switchView() {
     if(releasesSection.classList.contains('expanded')) {
     removeClass(releasesSection, 'expanded');
   }
   if(!infoDisplay.classList.contains('expanded')) {
     addClass(infoDisplay, 'expanded');
   }
-  portfolioItems = designItems;
+}
+
+
+listen("click", gameButton, () => {
+  addClass(portfolioWrapper, 'fade-out');
+  setTimeout(() => {
+  switchView();
+  portfolioItems = gameItems;
   displayPortfolioItem(currentIndex);
+    displayPortfolioItem(currentIndex);
+    removeClass(portfolioWrapper, 'fade-out');
+  }, 300);
   return portfolioItems;
 });
+
+listen("click", designButton, () => {
+  addClass(portfolioWrapper, 'fade-out');
+  setTimeout(() => {
+  switchView();
+  portfolioItems = designItems;
+  displayPortfolioItem(currentIndex);
+    displayPortfolioItem(currentIndex);
+    removeClass(portfolioWrapper, 'fade-out');
+  }, 300);
+  return portfolioItems;
+});
+
 listen("click", releasesButton, () => {
   if(!releasesSection.classList.contains('expanded')) {
     addClass(releasesSection, 'expanded');
@@ -112,13 +197,21 @@ listen("click", releasesButton, () => {
 });
 
 listen("click", leftButton, () => {
-  currentIndex = adjustIndex(currentIndex, '-');
-  displayPortfolioItem(currentIndex);
+  addClass(portfolioWrapper, 'fade-out');
+  currentIndex = adjustIndex(currentIndex, '-', portfolioItems.length);
+  setTimeout(() => {
+    displayPortfolioItem(currentIndex);
+    removeClass(portfolioWrapper, 'fade-out');
+  }, 300);
 });
 
 listen("click", rightButton, () => {
-  currentIndex = adjustIndex(currentIndex, '+');
-  displayPortfolioItem(currentIndex);
+  addClass(portfolioWrapper, 'fade-out');
+  currentIndex = adjustIndex(currentIndex, '+', portfolioItems.length);
+  setTimeout(() => {
+    displayPortfolioItem(currentIndex);
+    removeClass(portfolioWrapper, 'fade-out');
+  }, 300);
 });
 
 
